8b8d850508024c9874a6d9cc2333e0bd2c5e3c85
Theo Paris <theoplaysmc@gmail.com>
2020-03-23 08:11:49 -0700

    test

--- src/routes/blog/blog.ts
+++ src/routes/blog/blog.ts
@@ -7,18 +7,20 @@ const router = express.Router()
 
 router.use('/api', apiRouter)
 
-router.use('/posts', async (req, res) => {
-  res.render('blog/blog', { posts: await posts.find({}, { sort: { createdAt: -1 } }) })
-})
-
-router.use('/', async (req, res) => {
-  res.redirect('/blog/posts')
-})
-
 router.use('/indiv/:title', async (req, res) => {
   res.render('blog/individual', {
     post: await posts.findOne({ title: req.params.title }),
   })
+})
+
+router.use('/posts', async (req, res) => {
+  res.render('blog/blog', {
+    posts: await posts.find({}, { sort: { createdAt: -1 } }),
+  })
+})
+
+router.use('/', async (req, res) => {
+  res.redirect('/blog/posts')
 })
 
 export const blogRouter = router
