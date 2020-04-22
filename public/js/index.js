adcf699de49115345358d63b99b590d3d8aded5f
Theo Paris <theoplaysmc@gmail.com>
2020-04-22 12:49:16 -0700

    added animate.css for header animations

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,29 @@
-$(function() {
-  /**
-   * Jquery code to insert header code into a blank <header> tag.
-   *
-   * Using ejs now, this is not needed!
-   */
-  // $("header").load("/components/header");
+$(function () {
+    /**
+     * Jquery code to insert header code into a blank <header> tag.
+     *
+     * Using ejs now, this is not needed!
+     */
+    // $("header").load("/components/header");
 
-  // Feather Icons
-  setTimeout(() => feather.replace(), 500)
-  if ($('#error')) $('#error').fadeOut(5000)
+    // Feather Icons
+    setTimeout(() => feather.replace(), 500)
+    $('#brand-name').hover(
+        function () {
+            $(this).addClass('animated tada')
+        },
+        function () {
+            $(this).removeClass('animated tada')
+        }
+    )
+    $('.navlink').hover(
+        function () {
+            $(this).addClass('animated rubberBand')
+        },
+        function () {
+            $(this).removeClass('animated rubberBand')
+        }
+    )
+
+    if ($('#error')) $('#error').fadeOut(5000)
 })
