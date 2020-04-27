4d1d6792b0674857a77ffd5775e36fd4ed66bb9b
Theo Paris <theoplaysmc@gmail.com>
2020-04-27 16:22:27 -0700

    - added pnpm lock - added footer component

--- src/routes/throw-out-error.ts
+++ src/routes/throw-out-error.ts
@@ -4,17 +4,21 @@ import fetch from 'node-fetch'
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send(
-    '<h1>Throw Out Error</h1><p>This website is work in progress.</p>'
-  )
+    res.send('<h1>Throw Out Error</h1><p>This website is work in progress.</p>')
 })
 
-router.get("/projects", async (req, res) => {
+router.get('/projects', async (req, res) => {
     // Fetch github projects from my dev team
-    var githubProjects = await (await fetch("https://api.github.com/users/throw-out-error/repos")).json();
+    var githubProjects = await (
+        await fetch('https://api.github.com/users/throw-out-error/repos')
+    ).json()
     res.render('projects/index', {
-      projectsWorkedOn: githubProjects.length,
+        projectsWorkedOn: githubProjects.length,
     })
-});
+})
+
+router.get('/git', (req, res) =>
+    res.redirect('https://github.com/throw-out-error')
+)
 
 export const throwOutErrorRouter = router
