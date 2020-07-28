3a46e654ac554067c0ab431602643ddb81bcf71d
Theo Paris <theoparisdesigns@gmail.com>
2020-07-27 19:24:44 -0700

    wip - moved server to another repo

--- packages/server/public/js/dashboard.js
+++
@@ -1,1 +0,0 @@
-// Test for connecting to server
--- packages/server/src/server.ts
+++
@@ -1,6 +0,0 @@
-import { App } from './index'
-const port = process.env.SERVER_PORT || 8080
-
-App.listen(port, () => {
-    console.log(`Express server listening on 0.0.0.0:${port}`)
-})
