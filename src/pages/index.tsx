7c7e2b23695a7ebfe37223b3c859959641260ba8
Theo Paris <theoparisdesigns@gmail.com>
2021-03-04 19:03:13 -0800

    Fixed react-pag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,14 @@ import { MarkdownPage } from "@flowtr/react-page";
 import React from "react";
 import page from "./workspace.md";
 
-export const WorkspacePage = () => <MarkdownPage path={page} purify={true}
-                                                 render={{
-                                                     title: true,
-                                                     date: {
-                                                         weekday: "long",
-                                                         year: "numeric",
-                                                         month: "long",
-                                                         day: "numeric",
-                                                         prefix: "Updated ",
-                                                     },
-                                                     subtitle: true,
-                                                 }} />;
+export const WorkspacePage = () => (
+    <MarkdownPage
+        path={page}
+        purify={true}
+        render={{
+            title: true,
+            date: false,
+            subtitle: true,
+        }}
+    />
+);
