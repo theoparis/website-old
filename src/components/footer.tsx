a2011cc92ffaf07733d9101602b2efccae1c1137
Theo Paris <theoparisdesigns@gmail.com>
2021-06-03 17:46:51 -0700

    Huge theme update

---
+++ src/components/footer.tsx
@@ -0,0 +1,18 @@
+import { Flex, Text } from "@chakra-ui/react";
+import { Section } from "./section";
+
+export const Footer = () => {
+    return (
+        <Section
+            pos="static"
+            w="100%"
+            p="1.5em"
+            as="footer"
+            bottom="0"
+            bg="bg.secondary"
+            skipMargin
+        >
+            <Text>Made By Theo Paris</Text>
+        </Section>
+    );
+};
